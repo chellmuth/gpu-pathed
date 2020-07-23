387610f3d3c3983cc5f69cf6e508e41fdefccc9a
Chris Hellmuth <chellmuth@gmail.com>
2020-07-23 09:26:17 -0700

    Cleanup header file

--- include/scene.h
+++ include/scene.h
@@ -14,38 +14,6 @@ namespace rays {
 
 constexpr float defaultLightPosition = -0.6f;
 
-namespace SceneParameters {
-
-inline SceneData getSceneData(int index) {
-    if (index == 0) {
-        std::string sceneFilename("../scenes/cornell-box/CornellBox-Original.obj");
-        ObjParser objParser(sceneFilename);
-        return SceneAdapter::createSceneData(objParser);
-    } else {
-        return SceneAdapter::createSceneData(defaultLightPosition);
-    }
-}
-
-inline Camera getCamera(int index, Resolution resolution) {
-    if (index == 0) {
-        return Camera(
-            Vec3(0.f, 1.f, 6.8f),
-            Vec3(0.f, 1.f, 0.f),
-            19.5f / 180.f * M_PI,
-            resolution
-        );
-    } else {
-        return Camera(
-            Vec3(0.f, 0.3f, 5.f),
-            Vec3(0.f),
-            30.f / 180.f * M_PI,
-            resolution
-        );
-    }
-}
-
-}
-
 class Scene {
 public:
     Scene(
@@ -79,3 +47,11 @@ private:
 };
 
 }
+
+
+namespace rays { namespace SceneParameters {
+
+SceneData getSceneData(int index);
+Camera getCamera(int index, Resolution resolution);
+
+} }
--- src/scene.cu
+++ src/scene.cu
@@ -1,6 +1,40 @@
 #include "scene.h"
 namespace rays {
 
+namespace SceneParameters {
+
+SceneData getSceneData(int index)
+{
+    if (index == 0) {
+        std::string sceneFilename("../scenes/cornell-box/CornellBox-Original.obj");
+        ObjParser objParser(sceneFilename);
+        return SceneAdapter::createSceneData(objParser);
+    } else {
+        return SceneAdapter::createSceneData(defaultLightPosition);
+    }
+}
+
+Camera getCamera(int index, Resolution resolution)
+{
+    if (index == 0) {
+        return Camera(
+            Vec3(0.f, 1.f, 6.8f),
+            Vec3(0.f, 1.f, 0.f),
+            19.5f / 180.f * M_PI,
+            resolution
+        );
+    } else {
+        return Camera(
+            Vec3(0.f, 0.3f, 5.f),
+            Vec3(0.f),
+            30.f / 180.f * M_PI,
+            resolution
+        );
+    }
+}
+
+}
+
 void Scene::init()
 {
     update();
