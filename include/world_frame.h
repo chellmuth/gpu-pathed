bf9eea31db3f54cda5f1c5ceaeb3dd7294c13524
Chris Hellmuth <chellmuth@gmail.com>
2020-07-29 11:03:49 -0700

    Forgot to add WorldFrame header

---
+++ include/world_frame.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "vec3.h"
+
+namespace rays { namespace WorldFrame {
+    __device__ inline float cosTheta(Vec3 normal, Vec3 w)
+    {
+        return fmaxf(
+            0.f,
+            dot(normal, w)
+        );
+    }
+
+    __device__ inline float absCosTheta(Vec3 normal, Vec3 w)
+    {
+        return fabsf(cosTheta(normal, w));
+    }
+} };
