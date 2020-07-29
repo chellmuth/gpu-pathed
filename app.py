f10d106c3684884e2ae499e0cfee2d551afd0338
Chris Hellmuth <chellmuth@gmail.com>
2020-07-29 12:53:45 -0700

    Refactor RenderWidget into separate files

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import sys
 
-import OpenGL.GL as gl
 from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtWidgets import (
     QApplication,
     QGroupBox,
     QHBoxLayout,
     QLabel,
-    QOpenGLWidget,
     QVBoxLayout,
     QWidget
 )
@@ -15,6 +13,7 @@ from PyQt5.QtWidgets import (
 import path_tracer
 from camera_widget import CameraWidget
 from material_widget import MaterialWidget
+from render_widget import RenderWidget
 from settings_widget import SettingsWidget
 
 class App(QWidget):
@@ -47,60 +46,6 @@ class App(QWidget):
     def update(self):
         self.gl.update()
         self.sidebar.update()
-
-
-class RenderWidget(QOpenGLWidget):
-    def __init__(self, pt, handleColorChange, parent=None):
-        super().__init__(parent)
-
-        self.pt = pt
-        self.handleColorChange = handleColorChange
-        self.setFixedSize(pt.getWidth(), pt.getHeight())
-
-    def mousePressEvent(self, event):
-        self.pt.hitTest(event.x(), self.height() - event.y() - 1)
-        self.handleColorChange
-
-    def wheelEvent(self, event):
-        ticks = event.angleDelta().y() / 120
-        self.pt.getSceneModel().zoomCamera(ticks)
-
-    def initializeGL(self):
-        super().initializeGL()
-
-        width = self.width()
-        height = self.height()
-
-        gl.glClearColor(1., 0.5, 1., 1.)
-
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-
-        self.pbo1, self.pbo2 = gl.glGenBuffers(2)
-
-        for pbo in [self.pbo1, self.pbo2]:
-            gl.glBindBuffer(gl.GL_PIXEL_UNPACK_BUFFER, pbo)
-            gl.glBufferData(
-                gl.GL_PIXEL_UNPACK_BUFFER,
-                4 * width * height,
-                None,
-                gl.GL_DYNAMIC_DRAW
-            )
-            gl.glBindBuffer(gl.GL_PIXEL_UNPACK_BUFFER, 0)
-
-        self.state = self.pt.init(self.pbo1, self.pbo2)
-
-    def paintGL(self):
-        if self.state.isRendering:
-            self.state = self.pt.pollRender()
-        else:
-            self.state = self.pt.renderAsync()
-
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-        gl.glBindBuffer(gl.GL_PIXEL_UNPACK_BUFFER, self.state.pbo)
-
-        gl.glDrawPixels(self.width(), self.height(), gl.GL_RGBA, gl.GL_UNSIGNED_BYTE, None)
-
-        gl.glBindBuffer(gl.GL_PIXEL_UNPACK_BUFFER, 0)
 
 
 class Sidebar(QWidget):
